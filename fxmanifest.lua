e5fa1e06b70f7d60349feed3b64cdfb7ec2d84ae
X00LA <X00LA@users.noreply.github.com>
2021-06-14 13:30:30 +0200

    Load language files
    Updated to load all language files.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,30 +8,14 @@ version '1.1.0'
 
 server_scripts {
 	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/es.lua',
-	'locales/ru.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/cs.lua',
-	'locales/pl.lua',
+	'locales/*.lua',
 	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/es.lua',
-	'locales/ru.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/cs.lua',
-	'locales/pl.lua',
+	'locales/*.lua',
 	'config.lua',
 	'client/main.lua'
 }
